68ebaa8ef8507df2771bea0843710f8c4122e76b
Liam DeVoe <orionldevoe@gmail.com>
2021-10-15 22:29:31 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from setuptools import find_packages
+import re
+
+# https://stackoverflow.com/a/7071358
+VERSION = "Unknown"
+VERSION_RE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+
+with open("denpa_finder/version.py") as f:
+    match = re.search(VERSION_RE, f.read())
+    if match:
+        VERSION = match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string in "
+            "denpa_finder/version.py")
+
+setup(
+    name = "denpa_finder",
+    version = VERSION,
+    packages = find_packages()
+)
